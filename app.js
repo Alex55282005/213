c5f0a558ef386a9cd557cace286f54c159a0601e
Oleh Shevchenko <oleg.shavchenko.2005@gmail.com>
2024-04-27 02:27:30 +0300

    first

---
+++ app.js
@@ -0,0 +1,34 @@
+// Функция для установки куки
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Функция для получения куки по имени
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+// Пример использования: сохраняем имя пользователя в куки на 30 дней
+var userName = "John Doe";
+setCookie("username", userName, 30);
+
+// Получаем значение из куки и выводим его в консоль
+var storedUserName = getCookie("username");
+console.log("Stored username:", storedUserName);
